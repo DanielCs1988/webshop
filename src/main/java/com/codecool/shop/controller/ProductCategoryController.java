0cac9122a591b07c9a2294e2accee4a725a88e50
mbalazs9001 <mbalazs9001@gmail.comgit config --global user.email mbalazs9001@gmail.comgit config --global user.email mbalazs9001@gmail.com>
2018-04-24 13:08:27 +0200

    JSONified ProductCategory array

--- src/main/java/com/codecool/shop/controller/ProductCategoryController.java
+++ src/main/java/com/codecool/shop/controller/ProductCategoryController.java
@@ -1,20 +1,25 @@
 package com.codecool.shop.controller;
 
 import com.codecool.shop.dao.ProductCategoryDao;
-import com.codecool.shop.dao.ProductDao;
 import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
-import com.codecool.shop.dao.implementation.ProductDaoMem;
+import com.google.gson.Gson;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(urlPatterns = {"/product-category"})
 public class ProductCategoryController extends HttpServlet {
 
+    private Gson gson = new Gson();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+        PrintWriter out = resp.getWriter();
+        out.println(gson.toJson(productCategoryDataStore.getAll()));
     }
-
 }
+

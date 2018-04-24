3afe7933ff81b509da7151f949cffd67a06db522
Adam Feher <fhradm@gmail.com>
2018-04-24 14:41:35 +0200

    Renamed shoppingcart add method

--- src/main/java/com/codecool/shop/controller/ShoppingCartController.java
+++ src/main/java/com/codecool/shop/controller/ShoppingCartController.java
@@ -1,8 +1,11 @@
 package com.codecool.shop.controller;
 
+import com.codecool.shop.dao.ShoppingCartDao;
 import com.codecool.shop.dao.SupplierDao;
+import com.codecool.shop.dao.implementation.ShoppingCartDaoMem;
 import com.codecool.shop.dao.implementation.SupplierDaoMem;
 import com.codecool.shop.model.Product;
+import com.codecool.shop.model.ShoppingCart;
 import com.codecool.shop.model.Supplier;
 import com.google.gson.Gson;
 
@@ -12,9 +15,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.List;
-import java.util.Map;
+
 
 @WebServlet(urlPatterns = {"/shopping-cart"})
 public class ShoppingCartController extends HttpServlet {
@@ -25,7 +26,9 @@ public class ShoppingCartController extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         ShoppingCartDao shoppingCartDataStore = ShoppingCartDaoMem.getInstance();
         String shoppingCart = req.getParameter("shopping-cart");
-        shoppingCartDataStore.setOrders(gson.fromJson(shoppingCart, ShoppingCart.class));
+        ShoppingCart order = gson.fromJson(shoppingCart, ShoppingCart.class);
+        shoppingCartDataStore.add(order);
+        System.out.println(order);
     }
 
 }

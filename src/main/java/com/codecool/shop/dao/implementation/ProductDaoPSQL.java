b7ad01682fc33c1d4fe28c8e0c0fabd6a8132ac4
DanielCs88 <Pg26cv%g37>
2018-05-08 13:54:23 +0200

    80% of ProductDAO complete.

--- src/main/java/com/codecool/shop/dao/implementation/ProductDaoPSQL.java
+++ src/main/java/com/codecool/shop/dao/implementation/ProductDaoPSQL.java
@@ -2,37 +2,59 @@ package com.codecool.shop.dao.implementation;
 
 import com.codecool.shop.dao.ModelAssembler;
 import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.utils.QueryProcessor;
 import com.codecool.shop.model.Product;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
 public class ProductDaoPSQL implements ProductDao {
 
+    private ModelAssembler<Product> assembler = rs -> new Product(
+            rs.getInt("id"),
+            rs.getString("name"),
+            rs.getString("description"),
+            rs.getFloat("price"),
+            rs.getString("currency"),
+            rs.getString("image"),
+            rs.getInt("product_category_id"),
+            rs.getInt("supplier_id")
+    );
+
     @Override
     public void add(Product product) {
-
+        QueryProcessor.ExecuteUpdate(
+                "INSERT INTO products (name, description, price, currency, image, product_category_id, supplier_id)",
+                product.getName(),
+                product.getDescription(),
+                String.valueOf(product.getPrice()),
+                product.getDefaultCurrency().toString(),
+                product.getImageName(),
+                String.valueOf(product.getProductCategory()),
+                String.valueOf(product.getSupplier())
+        );
     }
 
     @Override
     public Product find(int id) {
-        return null;
+        return QueryProcessor.FetchOne("SELECT * FROM products WHERE id = ?;", assembler, String.valueOf(id));
     }
 
     @Override
     public void remove(int id) {
-
+        QueryProcessor.ExecuteUpdate("DELETE FROM products WHERE id = ?;", String.valueOf(id));
     }
 
     @Override
     public List<Product> getAll() {
-        return null;
+        return QueryProcessor.FetchAll("SELECT * FROM products;", assembler);
     }
 
     @Override
     public List<Product> getBySupplier(int supplierId) {
-        return null;
+        return QueryProcessor.FetchAll(
+                "SELECT * FROM products WHERE supplier_id = ?;",
+                assembler, String.valueOf(supplierId)
+        );
     }
 
     @Override
@@ -44,22 +66,4 @@ public class ProductDaoPSQL implements ProductDao {
     public List<Product> getBySupplierAndCategory(int supplierId, int productCategoryId) {
         return null;
     }
-
-    private final class ProductAssembler implements ModelAssembler<Product> {
-
-        @Override
-        public Product assemble(ResultSet rs) throws SQLException {
-            return new Product(
-                    rs.getInt("id"),
-                    rs.getString("name"),
-                    rs.getString("description"),
-                    rs.getFloat("price"),
-                    rs.getString("currency"),
-                    rs.getString("image"),
-                    rs.getInt("product_category_id"),
-                    rs.getInt("supplier_id")
-            );
-        }
-    }
-
 }

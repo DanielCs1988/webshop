2a23e393153ebaa563fc073177090ce04a271dab
DanielCs88 <Pg26cv%g37>
2018-05-09 15:05:15 +0200

    ProductOrderDao update and delete.

--- src/main/java/com/codecool/shop/dao/implementation/ProductOrderDaoPSQL.java
+++ src/main/java/com/codecool/shop/dao/implementation/ProductOrderDaoPSQL.java
@@ -1,6 +1,7 @@
 package com.codecool.shop.dao.implementation;
 
 import com.codecool.shop.dao.ProductOrderDao;
+import com.codecool.shop.dao.utils.QueryProcessor;
 import com.codecool.shop.model.ProductOrder;
 
 import java.util.List;
@@ -14,12 +15,15 @@ public class ProductOrderDaoPSQL implements ProductOrderDao {
 
     @Override
     public void updateQuantity(int id, int quantity) {
-
+        QueryProcessor.ExecuteUpdate(
+                "UPDATE product_orders SET quantity = ? WHERE id = ?",
+                String.valueOf(quantity), String.valueOf(id)
+        );
     }
 
     @Override
     public void remove(int id) {
-
+        QueryProcessor.ExecuteUpdate("DELETE FROM product_orders WHERE id = ?", String.valueOf(id));
     }
 
     @Override

0b39c613cecef702d0909192e5f54139c19b6a4f
mbalazs9001 <mbalazs9001@gmail.comgit config --global user.email mbalazs9001@gmail.comgit config --global user.email mbalazs9001@gmail.com>
2018-05-10 11:24:24 +0200

    Created controller class for user registration

---
+++ src/main/java/com/codecool/shop/controller/UserRegisterController.java
@@ -0,0 +1,31 @@
+package com.codecool.shop.controller;
+
+
+import com.codecool.shop.dao.UserDao;
+import com.codecool.shop.dao.implementation.UserDaoPSQL;
+import com.codecool.shop.model.User;
+import com.google.gson.Gson;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/webshop/register"})
+public class UserRegisterController extends HttpServlet {
+
+    private Gson gson = new Gson();
+    private UserDao userDataStore = new UserDaoPSQL();
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String input = req.getParameter("user");
+        User newUser = gson.fromJson(input, User.class);
+        newUser.setPassword(PasswordStorage.createHash(newUser.getPassword()));
+        userDataStore.add(newUser);
+
+    }
+}

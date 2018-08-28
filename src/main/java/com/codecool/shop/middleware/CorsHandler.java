6c95b027e44f73cfee419ecb5e4c08a1982beae5
DanielCs88 <daniel.cs20@gmail.com>
2018-08-28 16:25:08 +0200

    CORS middleware. Attempt 4.

--- src/main/java/com/codecool/shop/middleware/CorsHandler.java
+++ src/main/java/com/codecool/shop/middleware/CorsHandler.java
@@ -11,6 +11,16 @@ import java.io.PrintWriter;
 public class CorsHandler implements Filter {
 
     @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+    @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
         HttpServletRequest request = (HttpServletRequest) servletRequest;
